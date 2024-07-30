f3c7aef7bac1ec33bff9362b157c6185793fb3af
tokisuno <lucaswmei@gmail.com>
2024-07-30 10:27:48 -0400

    refactored, implementing dialogue box for username

--- game.js
+++ game.js
@@ -16,6 +16,7 @@ function Gameboard() {
         ["ba", "bb", "bc"],
         ["ca", "cb", "cc"]
     ];
+
     return { board, boardClasses };
 }
 
@@ -30,64 +31,20 @@ function Player(name, mark) {
     this.win = function() {
         this.score++;
         this.won = true;
-        display.removeChild(board);
         game.continue();
     }
 
-    this.tie = function() {
-        return;
-    }
-
-    this.checkWin = function(mark) {
-        let count = 0;
-        if (player1.moves.length === 5 && player2.moves.length === 4) {
-            this.tie();
-        }
-        
-        // horizontal
-        for (let row = 0; row < 3; row++) {
-            for (let col = 0; col < 3; col++) {
-                if (boardArray.board[row][col] === mark) {
-                    count++;
-                    console.log(boardArray.board[row][col], count);
-                    if (count === 3) {
-                        this.win();
-                        break;
-                    }
-                } else { count = 0; break; };
-            }
-            count = 0;
-        }
-
-        // vertical
-        for (let col = 0; col < 3; col++) {
-            for (let row = 0; row < 3; row++) {
-                if (boardArray.board[row][col] === mark) {
-                    count++;
-                    if (count === 3) {
-                        this.win();
-                        break;
-                    }
-                } else { count = 0; break; };
-            }
-            count = 0;
-        }
-
-        // cross
-        if (boardArray.board[0][0] === mark &&
-            boardArray.board[1][1] === mark &&
-            boardArray.board[2][2] === mark) {
-            this.win();
-        } 
-        if (boardArray.board[2][0] === mark &&
-            boardArray.board[1][1] === mark &&
-            boardArray.board[0][2] === mark) {
-            this.win();
-        }
-    }
 }
 
 function Game() {
+    this.tie = false;
+    this.checkTie = function() {
+        if (player1.moves.length === 5 && player2.moves.length === 4) {
+	    game.tie = true;
+	    game.continue();
+	    return;
+        }
+    }
     this.validMove = function(pos) {
         let p1check = player1.moves.indexOf(pos);
         let p2check = player2.moves.indexOf(pos);
@@ -95,7 +52,7 @@ function Game() {
             return true;
         }
     }
-
+    
     this.move = function(square) {
         if (player1.moved === false && player2.moved === false || player2.moved === true) {
             player1.lastMove = square.target.className;
@@ -121,7 +78,6 @@ function Game() {
         for (let i = 0; i < boardArray.boardClasses.length; i++) {
             for (let j = 0; j < boardArray.boardClasses.length; j++) {
                 if (boardArray.boardClasses[i][j] === pos) {
-                    console.log(boardArray.board[i][j]);
                     boardArray.board[i][j] = `${playerMoved.mark}`;
                 } 
             }
@@ -129,6 +85,54 @@ function Game() {
         playerMoved.moved = true;
         playerWait.moved = false;
     }
+
+    this.checkWin = function(player, mark) {
+        if (player1.moves.length === 5 && player2.moves.length === 4) {
+            return;
+        }
+        let count = 0;
+        
+        // horizontal
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                if (boardArray.board[row][col] === mark) {
+                    count++;
+                    if (count === 3) {
+                        player.win();
+                        break;
+                    }
+                } else { count = 0; break; };
+            }
+            count = 0;
+        }
+
+        // vertical
+        for (let col = 0; col < 3; col++) {
+            for (let row = 0; row < 3; row++) {
+                if (boardArray.board[row][col] === mark) {
+                    count++;
+                    if (count === 3) {
+                        player.win();
+                        break;
+                    }
+                } else { count = 0; break; };
+            }
+            count = 0;
+        }
+
+        // cross
+        if (boardArray.board[0][0] === mark &&
+            boardArray.board[1][1] === mark &&
+            boardArray.board[2][2] === mark) {
+            player.win();
+        } 
+        if (boardArray.board[2][0] === mark &&
+            boardArray.board[1][1] === mark &&
+            boardArray.board[0][2] === mark) {
+            player.win();
+        }
+    }
+
 
     this.continue = function() {
         const continueScreen = document.createElement("div");
@@ -141,21 +145,22 @@ function Game() {
         result.setAttribute("class", "result");
         if (player1.won === true) {
             result.textContent = `${player1.name} won!`;
+	    player1.won = false;
         }
         if (player2.won === true) {
             result.textContent = `${player2.name} won!`;
+	    player2.won = false;
         }
+	if (game.tie === true) {
+	    result.textContent = "Whoa, it's a tie!";
+	    game.tie = false;
+	}
 
         const againButton = document.createElement("button");
         againButton.setAttribute("class", "again");
         againButton.textContent = "Go again?";
 
-        const leaveButton = document.createElement("button");
-        leaveButton.setAttribute("class", "leave");
-        leaveButton.textContent = "Quit?";
-
         result.appendChild(againButton);
-        result.appendChild(leaveButton);
         continueScreen.appendChild(result);
 
         const buttons = document.querySelectorAll("button");
@@ -172,21 +177,19 @@ function Game() {
                         player2.moved = false;
                         display.appendChild(board);
                         break;
-                    case "leave":
-                        break;
                 }
             });
         });
     }
 }
 
-let player1 = new Player("Lucas", "X");
+let player1 = new Player("Player 1", "❌");
 let player1dom_name = document.querySelector("div.player1name");
 let player1dom_score = document.querySelector("div.player1score");
 player1dom_name.textContent = player1.name;
 player1dom_score.textContent = player1.score;
 
-let player2 = new Player("Fate", "O");
+let player2 = new Player("Player 2", "⭕");
 let player2dom_name = document.querySelector("div.player2name");
 let player2dom_score = document.querySelector("div.player2score");
 player2dom_name.textContent = player2.name;
@@ -209,7 +212,27 @@ const squares = document.querySelectorAll("div#square");
 squares.forEach((square) => {
     square.addEventListener("click", (square) => {
         game.move(square);
-        player1.checkWin(player1.mark);
-        player2.checkWin(player2.mark);
+        game.checkWin(player1, player1.mark);
+        game.checkWin(player2, player2.mark);
+	game.checkTie();
     });
 });
+
+const showBtn = document.querySelector("button.show-options");
+const dialog = document.querySelector("dialog#dialog");
+const submit = dialog.querySelector("input#submit");
+const userInput = dialog.querySelector("input#userInput");
+
+showBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+dialog.addEventListener("close", () => {
+    console.log(dialog.returnValue ==="default" ? "no return value" : `Return value: ${dialog.returnValue}`);
+});
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close(userInput.value);
+});
+
